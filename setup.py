1b41c1ce8f582d47f820751e0aa0286f55afaa94
ar <github@awinia.de>
2022-10-11 14:40:37 +0200

    #269 update setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
+from setuptools.config import read_configuration
+conf_dict = read_configuration("setup.cfg")
+PKG_NAME = conf_dict['metadata']['name']
+
 if __name__ == "__main__":
-    setup(use_scm_version=False)
+    setup(
+        use_scm_version={"write_to": f"{PKG_NAME}/_version.py"}
+    )
