fa34632bde58595eee53d01cb4dd7d804fae300a
ar <github@awinia.de>
2022-10-11 14:40:37 +0200

    #269 update setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
 
+from setuptools.config import read_configuration
+conf_dict = read_configuration("setup.cfg")
+PKG_NAME = conf_dict['metadata']['name']
+
 if __name__ == "__main__":
-    setup(use_scm_version=False)
+    setup(use_scm_version=True)
--- src/semi_ate_installer/cli.py
+++ src/semi_ate_installer/cli.py
@@ -1,3 +1,4 @@
+import click
 import pyfiglet
 
 from semi_ate_installer.state.state_machine import NewEnvSM
@@ -9,12 +10,17 @@ def print_semi_ate_installer_banner():
     print(ascii_banner)
 
 
-def main():
+@click.command()
+def run():
     print_semi_ate_installer_banner()
 
     state_machine = NewEnvSM()
     while state_machine.is_done() != State.Done:
         state_machine.next()
+
+
+def main():
+    run()
 
 
 if __name__ == "__main__":
