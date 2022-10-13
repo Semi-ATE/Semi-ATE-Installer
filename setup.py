1b61ae30c109cf564e89bb6dca61eba18976e260
ar <github@awinia.de>
2022-10-13 08:47:54 +0200

    #269 remove conda&mamba dep

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-from setuptools.config import read_configuration
-conf_dict = read_configuration("setup.cfg")
-PKG_NAME = conf_dict['metadata']['name']
 
 if __name__ == "__main__":
     setup(use_scm_version=True)
