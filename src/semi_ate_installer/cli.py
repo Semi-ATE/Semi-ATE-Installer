6b5a1ffd2a7f190d4a75477ba4f901d806323f95
ar <github@awinia.de>
2022-10-19 07:16:32 +0200

    #269 resolve review comments

--- src/semi_ate_installer/cli.py
+++ src/semi_ate_installer/cli.py
@@ -10,6 +10,10 @@ def print_semi_ate_installer_banner():
     print(ascii_banner)
 
 
+# feedstock build will try to execute the semi-ate-installer to verify a working executable
+# but as the installer doesn't behave as a CLI it starts and waits for user input
+# which will block for ever and the build terminates with an error
+# this shall extend the installer to behave as a CLI
 @click.command()
 def run():
     print_semi_ate_installer_banner()
